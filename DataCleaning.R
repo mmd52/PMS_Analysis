664e7f1045da7937e241008c593faa9b83c2ca89
Mohammed Topiwalla <mohammedtopiwalla@hotmail.com>
2018-02-23 19:59:06 +0100

    Add files via upload

---
+++ DataCleaning.R
@@ -0,0 +1,48 @@
+# Data Cleaning 
+# 21/02/2018
+
+# Loading all packages ============================================================
+source("Libraries.R")
+
+# Loading Data ============================================================
+dat<-read.table(file.choose(),header = T,sep = ";")
+View(head(dat))
+sum(dat$Quantity..units.)
+sum(dat$Turnover..EURO.)
+
+summary(dat)
+
+# Converting our NA's to 0 
+dat[is.na(dat)] <- 0
+
+# Making our categorical variables =====================================
+dat$Plant<-as.factor(dat$Plant)
+dat$Product.Line<-as.factor(dat$Product.Line)
+dat$Product.Type<-as.factor(dat$Product.Type)
+dat$Turnover.Range...000.EURO.<-as.factor(dat$Turnover.Range...000.EURO.)
+dat$Geographical.Area<-as.factor(dat$Geographical.Area)
+
+# Start Feature Engineering =============================================
+
+# Contribution Margin/Gross profit = revenues - cost of goods
+dat$Gross_Margin <- dat$Turnover..EURO.- dat$Cost.of.Goods.Sold..EURO.
+
+# operational Profit = revenues - TC (Overheads included)
+dat$Total_Overheads <- dat$Total.Cost.of.Distribution + dat$Total.Customer...Order.Management.Costs + dat$Customer.related.Issues.Costs + dat$Total.Operations.Costs 
+
+# Deleted portion
+###### dat$Transportations  + dat$Manage.Orders.to.Suppliers + dat$Inbound.Logistics + dat$Inspect.goods + dat$Manage.Foreign.Relationships
+
+dat$Operational_Profit <- dat$Turnover..EURO. - dat$Cost.of.Goods.Sold..EURO. - dat$Total_Overheads
+
+# ROS = op/revenues 
+
+dat$ROS <- dat$Operational_Profit/dat$Turnover..EURO.
+
+# Renaming few columns
+names(dat)<-c("Material_Code","Plant","Customer_Code","Quantity_units","Turnover_EURO","Cost_of_Goods_Sold_EURO","Total_Cost_of_Distribution","Sales_Transportation","Packing","Distribution_Activities","Trading_Transportation","Custom_Duties_Trading","Warehouse_Rent","Outbound_Logistics_Local_Branches","Total_Customer_Order_Management_Costs","Sales_Comissions","Sales_Development","Offer_Development","Order_Management","Customer_Service","Manage_Account_Receivables","Customer_Order_Management_Local_Branches","Customer_related_Issues_Costs","Rebate","Bad_Debt","Cost_of_Poor_Quality","Total_Operations_Costs","Manufacturing_Costs","Internal_Logistics_Costs","Supply_Chain_Management_Costs","Transportations","Manage_Orders_to_Suppliers","Inbound_Logistics","Inspect_goods","Manage_Foreign_Relationships","Product_Line","Product_Type","N_Parts","Assembly_Labour_Minutes","Machinery_Minutes","Number_of_Deliveries","Average_Delivery_Batch_Size_units","Customer_Class","Turnover_Range_EURO","Geographical_Area","Gross_Margin","Total_Overheads","Operational_Profit","ROS")
+
+# Data cleaning complete ======================================================================
+View(head(dat))
+summary(dat)
+write.csv(dat,"FinalPMSData.csv")
